872e1ee3996a8384952e3f16db89b2d82d46c2fd
serge <Aqesh228mnb123>
2022-05-10 12:41:08 +0300

    Homework done. 05.05.2022

--- main.py
+++ main.py
@@ -1,58 +1,47 @@
-'''
-Написать программу которая :
-1. Высчитывает возраст из заданных данных (current_year - нынешний год, year_of_birth - год рождения)
-#current_year = 2022
-#year_of_birth = 1988
-'''
-current_year = 2022
-year_of_birth = 1988
-
-a = int(input("Введите Ваш год рождения: "))
-b = int(input("Введите нынешний год: "))
-age = (b - a)
-print(age)
-
-# '''
-
-
-
-'''Найти недостающую часть кода (code_2)
-a. Найдите остаток от х деленого на у
-b. Полученный результат умножьте на 13
-c. Извлеките квадратный корень из полученного результата
-d. Возьмите целую часть от результата
-code_2 data
-x = 152
-y = 132 
-'''
-
-x = 152
-y = 132
-
-result_a = x % y
-result_b = result_a * 13
-result_c = result_b ** 0.5
-result_d = int(result_c)
-print(result_a, result_b, result_c, result_d)
-
-'''
- Соединить код в отдельную переменную пример:
-        475-12-967
-
-'''
-code_1 = '354' #str
-code_2 = result_d #int
-code_3 = 132 #int
-
-total_code = code_1 + "-" + str(code_2) + "-" + str(code_3)
-print(total_code)
-
-print("Hello Mary Gold. You are " + str(age) + " years old. Your secret code is " + total_code + ".")
-
-
-'''
-
-'''
-
-
-
+# # Write a code to return "Hero" from given string
+# example_string1 = "Hello bro"
+# print(example_string1[0] + example_string1[1] + example_string1[7] + example_string1[8])
+# # Write a code to return "Jack is my name"
+#
+# example_string2 = "jack Is My NAME"
+# x = example_string2.capitalize()
+# print(x)
+#
+# # Write a code to return "Get rid of stars please"
+# example_string3 = "*Get rid of stars please*"
+# x = example_string3.strip("*")
+# print(x)
+# #
+#
+# Write a code to return "Hello my name is Jack"
+# example_string4 = "hello my name is jack"
+# name = 'jack'
+# x = f'Hello my name is {name.capitalize()}'
+# print(x)
+#
+#
+# # Write a code to return formatted string "Hello, my name is Jack"
+# var1 = "jack"
+# var2 = "hello"
+# var3 = "MY NAME IS"
+# print(var2.capitalize() + ', ' +  var3.lower() + ' ' + var1.capitalize())
+#
+#
+#
+# Write a code to return byte_string text value
+# byte_string = b"\316\273"
+# print(byte_string.decode('utf-16'))
+#
+# # Write a code to return True if cost is greater than 500$
+# example_string5 = "It cost me 1000.00$"
+# example_string5 = example_string5.split()
+# print(example_string5)
+# cost = example_string5[3]
+# print(cost)
+# cost = cost.rstrip('$')
+# print(cost)
+# cost = float(cost)
+# print(type(cost))
+# if cost > 500:
+#     print(True)
+#
