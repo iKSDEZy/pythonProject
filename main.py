36b4febeb3b9597d596d299234ca923553c2fb7b
serge <Aqesh228mnb123>
2022-05-11 01:46:16 +0300

    Homework done. 11.05.2022

--- main.py
+++ main.py
@@ -1,47 +1,21 @@
-# # Write a code to return "Hero" from given string
-# example_string1 = "Hello bro"
-# print(example_string1[0] + example_string1[1] + example_string1[7] + example_string1[8])
-# # Write a code to return "Jack is my name"
-#
-# example_string2 = "jack Is My NAME"
-# x = example_string2.capitalize()
-# print(x)
-#
-# # Write a code to return "Get rid of stars please"
-# example_string3 = "*Get rid of stars please*"
-# x = example_string3.strip("*")
-# print(x)
-# #
-#
-# Write a code to return "Hello my name is Jack"
-# example_string4 = "hello my name is jack"
-# name = 'jack'
-# x = f'Hello my name is {name.capitalize()}'
-# print(x)
-#
-#
-# # Write a code to return formatted string "Hello, my name is Jack"
-# var1 = "jack"
-# var2 = "hello"
-# var3 = "MY NAME IS"
-# print(var2.capitalize() + ', ' +  var3.lower() + ' ' + var1.capitalize())
-#
-#
-#
-# Write a code to return byte_string text value
-# byte_string = b"\316\273"
-# print(byte_string.decode('utf-16'))
-#
-# # Write a code to return True if cost is greater than 500$
-# example_string5 = "It cost me 1000.00$"
-# example_string5 = example_string5.split()
-# print(example_string5)
-# cost = example_string5[3]
-# print(cost)
-# cost = cost.rstrip('$')
-# print(cost)
-# cost = float(cost)
-# print(type(cost))
-# if cost > 500:
-#     print(True)
-##
+# 0-100
+# Если число делится на 3 без остатка - написать число и Fizz
+# Если число делится на 5 без остатка - написать число и Buzz
+# Если число делится на 3 и на 5 без остатка - написать число и FizzBuzz
+
+
+spisok1 = list(range(0, 101))
+print(spisok1)
+for num in spisok1:
+    ostatok = num % 3
+    ostatok2 = num % 5
+    if ostatok + ostatok2 == 0:
+        print(num , 'FizzBuzz')
+    else:
+        if ostatok == 0:
+            print(num, 'Fizz')
+        if ostatok2 == 0:
+            print(num, 'Buzz')
+
+
+
